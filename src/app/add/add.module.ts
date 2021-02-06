f85708807da839dd0ec825c06640262e6a7259e3
Gabriel Paredes <gabriel@axented.com>
2021-02-06 14:59:33 -0500

    Fix add routing module

--- src/app/add/add.module.ts
+++ src/app/add/add.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddComponent } from './add.component';
+import { AddRoutingModule } from './add-routing.module';
 
 
 
 @NgModule({
   declarations: [AddComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AddRoutingModule
   ]
 })
 export class AddModule { }
