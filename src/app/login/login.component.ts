2116fa9c1a6dd9b457fe03cc8d418ba8395bbf4a
Gabriel Paredes <gabriel@axented.com>
2021-02-06 14:49:41 -0500

    Add login components for Google auth

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AngularFireAuth, public router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  googleLogin() {
+    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    .then((result) => {
+      this.router.navigate(['main'])
+    }).catch((error) => {
+      console.error(error)
+    })
   }
 
 }
