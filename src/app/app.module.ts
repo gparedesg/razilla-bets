f3173df89eb37250bcf7defe0210ccb4c463e4be
Gabriel Paredes <gabriel@axented.com>
2021-02-06 14:50:31 -0500

    Fix app module missing enviroment import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
